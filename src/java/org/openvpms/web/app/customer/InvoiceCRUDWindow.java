e04896b0d1cb304e3f388dbc444c0558cca81082
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2006-03-27 04:57:06 +0000

    Removed reverse button as per OVPMS-250: Customer Charges Workspace - remove reverse button action
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms-echo/trunk@686 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/web/app/customer/InvoiceCRUDWindow.java
+++ src/java/org/openvpms/web/app/customer/InvoiceCRUDWindow.java
@@ -1,27 +1,10 @@
 package org.openvpms.web.app.customer;
 
-import java.util.Date;
-
-import nextapp.echo2.app.Button;
 import nextapp.echo2.app.Row;
-import nextapp.echo2.app.event.ActionEvent;
-import nextapp.echo2.app.event.ActionListener;
 
-import org.openvpms.component.business.domain.im.archetype.descriptor.DescriptorException;
-import org.openvpms.component.business.domain.im.common.Act;
-import org.openvpms.component.business.domain.im.common.ActRelationship;
 import org.openvpms.component.business.domain.im.common.IMObject;
-import org.openvpms.component.business.domain.im.common.Participation;
-import org.openvpms.component.business.service.archetype.ArchetypeServiceException;
-import org.openvpms.component.business.service.archetype.IArchetypeService;
-import org.openvpms.web.app.subsystem.CRUDWindowListener;
-import org.openvpms.web.component.dialog.ErrorDialog;
 import org.openvpms.web.component.im.create.IMObjectCreator;
 import org.openvpms.web.component.im.create.IMObjectCreatorListener;
-import org.openvpms.web.component.im.edit.SaveHelper;
-import org.openvpms.web.component.im.util.IMObjectCopier;
-import org.openvpms.web.component.im.util.IMObjectCopyHandler;
-import org.openvpms.web.component.util.ButtonFactory;
 
 
 /**
@@ -31,16 +14,6 @@ import org.openvpms.web.component.util.ButtonFactory;
  * @version $LastChangedDate$
  */
 public class InvoiceCRUDWindow extends ActCRUDWindow {
-
-    /**
-     * The reverse button.
-     */
-    private Button _reverse;
-
-    /**
-     * Reverse button identifier.
-     */
-    private static final String REVERSE_ID = "reverse";
 
     /**
      * Invoice act type.
@@ -74,15 +47,9 @@ public class InvoiceCRUDWindow extends ActCRUDWindow {
      */
     @Override
     protected void layoutButtons(Row buttons) {
-        _reverse = ButtonFactory.create(REVERSE_ID, new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                onReverse();
-            }
-        });
         buttons.add(getEditButton());
         buttons.add(getCreateButton());
         buttons.add(getDeleteButton());
-        buttons.add(_reverse);
         buttons.add(getPrintButton());
     }
 
@@ -99,7 +66,6 @@ public class InvoiceCRUDWindow extends ActCRUDWindow {
             buttons.add(getEditButton());
             buttons.add(getCreateButton());
             buttons.add(getDeleteButton());
-            buttons.add(_reverse);
             buttons.add(getPrintButton());
         } else {
             buttons.add(getCreateButton());
@@ -121,119 +87,6 @@ public class InvoiceCRUDWindow extends ActCRUDWindow {
 
         String[] shortNames = {INVOICE_TYPE, CREDIT_TYPE};
         IMObjectCreator.create(getTypeDisplayName(), shortNames, listener);
-    }
-
-
-    /**
-     * Invoked when the 'reverse' button is pressed.
-     */
-    protected void onReverse() {
-        Act act = (Act) getObject();
-        String status = act.getStatus();
-        if (POSTED_STATUS.equals(status)) {
-            reverse(act);
-        } else {
-            showStatusError(act, "customer.invoice.noreverse.title",
-                            "customer.invoice.noreverse.message");
-        }
-    }
-
-    /**
-     * Reverse an invoice or credit act.
-     *
-     * @param act the act to reverse
-     */
-    private void reverse(Act act) {
-        try {
-            IMObjectCopier copier
-                    = new IMObjectCopier(new ReversalHandler(act));
-            Act reversal = (Act) copier.copy(act);
-            reversal.setStatus(INPROGRESS_STATUS);
-            reversal.setActivityStartTime(new Date());
-            setPrintStatus(reversal, false);
-            SaveHelper.save(reversal);
-            setObject(reversal);
-            CRUDWindowListener listener = getListener();
-            if (listener != null) {
-                listener.saved(reversal, false);
-            }
-        } catch (ArchetypeServiceException exception) {
-            ErrorDialog.show(exception);
-        } catch (DescriptorException exception) {
-            ErrorDialog.show(exception);
-        }
-    }
-
-
-    private static class ReversalHandler implements IMObjectCopyHandler {
-
-        /**
-         * Determines if the act is an invoice or a credit.
-         */
-        private final boolean _invoice;
-
-        /**
-         * Map of invoice types to their corresponding credit types.
-         */
-        private static final String[][] TYPE_MAP = {
-                {INVOICE_TYPE, CREDIT_TYPE},
-                {"act.customerInvoiceItem", "act.customerCreditItem"},
-                {"actRelationship.customerInvoiceItem",
-                 "actRelationship.customerCreditItem"}};
-
-
-        /**
-         * Construct a nedw <code>ReversalHandler</code>.
-         *
-         * @param act the act to reverse
-         */
-        public ReversalHandler(Act act) {
-            if (act.getArchetypeId().getShortName().equals(INVOICE_TYPE)) {
-                _invoice = true;
-            } else {
-                _invoice = false;
-            }
-        }
-
-        /**
-         * Determines how {@link IMObjectCopier} should treat an object.
-         *
-         * @param object  the source object
-         * @param service the archetype service
-         * @return <code>object</code> if the object shouldn't be copied,
-         *         <code>null</code> if it should be replaced with
-         *         <code>null</code>, or a new instance if the object should be
-         *         copied
-         */
-        public IMObject getObject(IMObject object, IArchetypeService service) {
-            IMObject result;
-            if (object instanceof Act || object instanceof ActRelationship
-                || object instanceof Participation) {
-                String replace = null;
-                String shortName = object.getArchetypeId().getShortName();
-                for (String[] map : TYPE_MAP) {
-                    String invoiceType = map[0];
-                    String creditType = map[1];
-                    if (_invoice) {
-                        if (invoiceType.equals(shortName)) {
-                            replace = creditType;
-                        }
-                    } else {
-                        if (creditType.equals(shortName)) {
-                            replace = invoiceType;
-                        }
-                    }
-                }
-                if (replace != null) {
-                    result = service.create(replace);
-                } else {
-                    result = service.create(shortName);
-                }
-            } else {
-                result = object;
-            }
-            return result;
-        }
     }
 
 }
