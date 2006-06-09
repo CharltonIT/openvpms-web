40ed2f6d5f23cefd8ed3c85ee4e5dc3ef70554e4
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2006-06-09 02:01:40 +0000

    Workaround for OBF-105
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@993 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/web/component/edit/IMObjectProperty.java
+++ src/java/org/openvpms/web/component/edit/IMObjectProperty.java
@@ -39,11 +39,20 @@ import org.openvpms.web.component.im.edit.ValidationHelper;
 import org.openvpms.web.resource.util.Messages;
 import org.openvpms.web.spring.ServiceHelper;
 
+import org.openvpms.component.business.dao.hibernate.im.entity.IMObjectDAOHibernate;
 import org.openvpms.component.business.domain.im.archetype.descriptor.NodeDescriptor;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 import org.openvpms.component.business.service.archetype.ValidationError;
 import org.openvpms.component.business.service.archetype.ValidationException;
+import org.openvpms.component.system.common.exception.OpenVPMSException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LazyInitializationException;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -89,6 +98,11 @@ public class IMObjectProperty implements Property, CollectionProperty {
      */
     private PropertyTransformer _transformer;
 
+    /**
+     * The logger.
+     */
+    private static final Log _log = LogFactory.getLog(IMObjectProperty.class);
+
 
     /**
      * Construct a new <code>IMObjectProperty</code>.
@@ -116,11 +130,22 @@ public class IMObjectProperty implements Property, CollectionProperty {
      * @return the collection
      */
     public Collection getValues() {
-        List<IMObject> values = _descriptor.getChildren(_object);
-        if (values != null) {
-            values = Collections.unmodifiableList(values);
+        try {
+            List<IMObject> values = _descriptor.getChildren(_object);
+            if (values != null) {
+                values = Collections.unmodifiableList(values);
+            }
+            return values;
+        } catch (OpenVPMSException exception) {
+            Throwable cause = exception.getCause();
+            if (cause instanceof LazyInitializationException) {
+                // @todo - workaround for OBF-105
+                return getValuesInSession();
+            } else {
+                _log.error(exception, exception);
+            }
         }
-        return values;
+        return null;
     }
 
     /**
@@ -370,4 +395,32 @@ public class IMObjectProperty implements Property, CollectionProperty {
                             + getDescriptor().getDisplayName());
         }
     }
+
+    /**
+     * Helper to get the collection values in a current hibernate session.
+     * todo: this is a hack to workaround OBF-105
+     *
+     * @return the collection or <code>null</code>
+     */
+    private Collection getValuesInSession() {
+        ApplicationContext context = ServiceHelper.getContext();
+        IMObjectDAOHibernate dao
+                = (IMObjectDAOHibernate) context.getBean("imObjectDao");
+        SessionFactory factory = dao.getHibernateTemplate().getSessionFactory();
+        Session session = factory.openSession();
+        try {
+            session.refresh(_object);
+            List<IMObject> values = _descriptor.getChildren(_object);
+            if (values != null) {
+                values = Collections.unmodifiableList(values);
+            }
+            return values;
+        } catch (Throwable throwable) {
+            _log.error(throwable, throwable);
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+
 }
