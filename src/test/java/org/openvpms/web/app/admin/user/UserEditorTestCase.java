6cbdbe6cfc0d9df55617e379d9bc9cb28f758979
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2011-11-16 04:35:22 +0000

    re-add UserEditorTestCase after merging conflicting changes from trunk
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.6@4499 9e4fa352-aafb-0310-9071-df624684024b

---
+++ src/test/java/org/openvpms/web/app/admin/user/UserEditorTestCase.java
@@ -0,0 +1,59 @@
+/*
+ *  Version: 1.0
+ *
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2011 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id: $
+ */
+
+package org.openvpms.web.app.admin.user;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.openvpms.archetype.rules.user.UserArchetypes;
+import org.openvpms.component.business.domain.im.security.User;
+import org.openvpms.web.component.im.layout.DefaultLayoutContext;
+import org.openvpms.web.test.AbstractAppTest;
+
+
+/**
+ * Tests the {@link UserEditor} class.
+ *
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate: $
+ */
+public class UserEditorTestCase extends AbstractAppTest {
+
+    /**
+     * Tests validation.
+     */
+    @Test
+    public void testValidate() {
+        User user = (User) create(UserArchetypes.USER);
+        UserEditor editor = new UserEditor(user, null, new DefaultLayoutContext());
+        assertFalse(editor.isValid());
+
+        editor.setUsername("foo");
+        assertFalse(editor.isValid());
+
+        editor.setPassword("bar");
+        assertFalse(editor.isValid());
+
+        editor.setName("Foo");
+        assertTrue(editor.isValid());
+
+        editor.setPassword(null);
+        assertFalse(editor.isValid());
+    }
+}
