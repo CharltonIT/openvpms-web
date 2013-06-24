667bf75a83348cea0f9017f98d86fe52c04726cd
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-06-24 03:20:27 +0000

    Minor reformat
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5196 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/act/ParticipationCollectionEditor.java
+++ openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/act/ParticipationCollectionEditor.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.edit.act;
@@ -30,30 +28,25 @@ import org.openvpms.web.component.property.CollectionProperty;
 /**
  * Editor for collections of {@link Participation}s.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate:2006-02-21 03:48:29Z $
+ * @author Tim Anderson
  */
 public class ParticipationCollectionEditor extends DelegatingCollectionEditor {
 
     /**
-     * Constructs a new <tt>ParticipationCollectionEditor</tt>.
+     * Constructs a {@link ParticipationCollectionEditor}.
      *
      * @param property the collection property
      * @param object   the parent object
      * @param context  the layout context
      */
-    public ParticipationCollectionEditor(CollectionProperty property,
-                                         IMObject object,
-                                         LayoutContext context) {
+    public ParticipationCollectionEditor(CollectionProperty property, IMObject object, LayoutContext context) {
         String[] shortNames = property.getArchetypeRange();
         int max = property.getMaxCardinality();
         AbstractIMObjectCollectionEditor editor;
         if (max == 1 && shortNames.length == 1) {
-            editor = new SingleParticipationCollectionEditor(property, object,
-                                                             context);
+            editor = new SingleParticipationCollectionEditor(property, object, context);
         } else {
-            editor = new DefaultIMObjectCollectionEditor(property, object,
-                                                         context);
+            editor = new DefaultIMObjectCollectionEditor(property, object, context);
         }
         setEditor(editor);
     }
--- openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/act/SingleParticipationCollectionEditor.java
+++ openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/act/SingleParticipationCollectionEditor.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.edit.act;
@@ -30,30 +28,27 @@ import org.openvpms.web.component.property.CollectionProperty;
  * Editor for collections of {@link Participation}s with 0..1 or 1..1
  * cardinality.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate:2006-02-21 03:48:29Z $
+ * @author Tim Anderson
  */
 public class SingleParticipationCollectionEditor
-    extends SingleIMObjectCollectionEditor {
+        extends SingleIMObjectCollectionEditor {
 
     /**
-     * Creates a new <tt>SingleParticipationCollectionEditor</tt>.
+     * Constructs a {@link SingleParticipationCollectionEditor}.
      *
      * @param property the collection property
      * @param object   the parent object
      * @param context  the layout context
      */
-    public SingleParticipationCollectionEditor(CollectionProperty property,
-                                               IMObject object,
-                                               LayoutContext context) {
+    public SingleParticipationCollectionEditor(CollectionProperty property, IMObject object, LayoutContext context) {
         super(property, object, context);
     }
 
     /**
-     * Returns the participation editor, or <tt>null</tt> if there is
+     * Returns the participation editor, or {@code null} if there is
      * no current participation editor
      *
-     * @return the participation editor. May be <tt>null</tt>
+     * @return the participation editor. May be {@code null}
      */
     protected ParticipationEditor getParticipationEditor() {
         IMObjectEditor editor = getCurrentEditor();
@@ -65,12 +60,11 @@ public class SingleParticipationCollectionEditor
      * Determines if the participation is empty. This returns true if
      * the participation entity is null and the participation is optional.
      *
-     * @return <tt>true</tt> if the participation is null
+     * @return {@code true} if the participation is null
      */
     protected boolean isEmpty() {
         ParticipationEditor editor = getParticipationEditor();
-        return editor != null && getCollection().getMinCardinality() == 0
-               && editor.isNull();
+        return editor != null && getCollection().getMinCardinality() == 0 && editor.isNull();
     }
 
 }
