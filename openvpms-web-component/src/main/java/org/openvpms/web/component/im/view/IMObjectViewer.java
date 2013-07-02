03fac545f12be3b5c1ee6a534954eb869415f2b5
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-07-02 06:40:25 +0000

    Enable support to register a ContextSwitchListener to IMObjectViewer
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5214 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-component/src/main/java/org/openvpms/web/component/im/view/IMObjectViewer.java
+++ openvpms-web-component/src/main/java/org/openvpms/web/component/im/view/IMObjectViewer.java
@@ -1,23 +1,24 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.view;
 
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.service.archetype.helper.DescriptorHelper;
+import org.openvpms.web.component.app.ContextSwitchListener;
 import org.openvpms.web.component.im.layout.DefaultLayoutContext;
 import org.openvpms.web.component.im.layout.IMObjectLayoutStrategy;
 import org.openvpms.web.component.im.layout.LayoutContext;
@@ -83,6 +84,17 @@ public class IMObjectViewer extends AbstractIMObjectView {
      */
     public String getTitle() {
         return DescriptorHelper.getDisplayName(getObject());
+    }
+
+    /**
+     * Registers a listener for context switch events.
+     * <p/>
+     * Note that this will only be utilised if set prior to {@link #getComponent()} being invoked.
+     *
+     * @param listener the listener. May be {@code null}
+     */
+    public void setContextSwitchListener(ContextSwitchListener listener) {
+        context.setContextSwitchListener(listener);
     }
 
     /**
