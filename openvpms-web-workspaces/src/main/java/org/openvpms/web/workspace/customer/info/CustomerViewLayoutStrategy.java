3c7ce781859639ccca06ca0e93a7d620b13d3acf
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-07-16 13:37:45 +0000

    Fixed rendering of CustomerAppointmentTableModel
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5310 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/customer/info/CustomerViewLayoutStrategy.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/customer/info/CustomerViewLayoutStrategy.java
@@ -1,35 +1,37 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2009 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 package org.openvpms.web.workspace.customer.info;
 
 import nextapp.echo2.app.Component;
 import org.openvpms.component.business.domain.im.act.Act;
-import org.openvpms.component.business.domain.im.archetype.descriptor.NodeDescriptor;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.party.Party;
 import org.openvpms.web.component.app.ContextSwitchListener;
 import org.openvpms.web.component.im.layout.AbstractLayoutStrategy;
+import org.openvpms.web.component.im.layout.DefaultLayoutContext;
 import org.openvpms.web.component.im.layout.LayoutContext;
 import org.openvpms.web.component.im.query.Browser;
 import org.openvpms.web.component.im.query.BrowserListener;
 import org.openvpms.web.component.im.query.DefaultIMObjectTableBrowser;
 import org.openvpms.web.component.im.query.Query;
 import org.openvpms.web.component.im.table.IMTableModel;
-import org.openvpms.web.component.property.PropertySet;
+import org.openvpms.web.component.im.view.TableComponentFactory;
+import org.openvpms.web.component.property.Property;
 import org.openvpms.web.echo.factory.ColumnFactory;
+import org.openvpms.web.echo.style.Styles;
 import org.openvpms.web.echo.tabpane.TabPaneModel;
 import org.openvpms.web.resource.i18n.Messages;
 
@@ -46,19 +48,18 @@ public class CustomerViewLayoutStrategy extends AbstractLayoutStrategy {
     /**
      * Lays out child components in a tab model.
      *
-     * @param object      the parent object
-     * @param descriptors the property descriptors
-     * @param properties  the properties
-     * @param model       the tab model
-     * @param context     the layout context
-     * @param shortcuts   if <tt>true</tt> include short cuts
+     * @param object     the parent object
+     * @param properties the properties
+     * @param model      the tab model
+     * @param context    the layout context
+     * @param shortcuts  if <tt>true</tt> include short cuts
      */
     @Override
-    protected void doTabLayout(IMObject object, List<NodeDescriptor> descriptors, PropertySet properties,
-                               TabPaneModel model, LayoutContext context, boolean shortcuts) {
-        super.doTabLayout(object, descriptors, properties, model, context, shortcuts);
+    protected void doTabLayout(IMObject object, List<Property> properties, TabPaneModel model, LayoutContext context,
+                               boolean shortcuts) {
+        super.doTabLayout(object, properties, model, context, shortcuts);
         Browser<Act> appointments = getAppointments((Party) object, context);
-        Component inset = ColumnFactory.create("Inset", appointments.getComponent());
+        Component inset = ColumnFactory.create(Styles.INSET, appointments.getComponent());
 
         String label = Messages.get("customer.info.appointments");
         if (shortcuts && model.size() < 10) {
@@ -76,7 +77,9 @@ public class CustomerViewLayoutStrategy extends AbstractLayoutStrategy {
      */
     protected Browser<Act> getAppointments(Party customer, LayoutContext context) {
         Query<Act> query = new CustomerAppointmentQuery(customer);
-        IMTableModel<Act> model = new CustomerAppointmentTableModel(context);
+        LayoutContext subContext = new DefaultLayoutContext(context);
+        subContext.setComponentFactory(new TableComponentFactory(subContext));
+        IMTableModel<Act> model = new CustomerAppointmentTableModel(subContext);
         Browser<Act> browser = new DefaultIMObjectTableBrowser<Act>(query, model, context);
         final ContextSwitchListener listener = context.getContextSwitchListener();
         browser.addBrowserListener(new BrowserListener<Act>() {
