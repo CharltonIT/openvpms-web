bdb7c2cbe5bfd43072d045cd085066f457c97713
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-09-21 06:00:13 +0000

    Changes for OVPMS-1085 Streamline Check-In
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5456 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/product/ProductCRUDWindow.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/product/ProductCRUDWindow.java
@@ -16,7 +16,13 @@
 
 package org.openvpms.web.workspace.product;
 
+import nextapp.echo2.app.Column;
+import nextapp.echo2.app.Component;
+import nextapp.echo2.app.Label;
+import nextapp.echo2.app.Row;
+import nextapp.echo2.app.SelectField;
 import nextapp.echo2.app.event.ActionEvent;
+import nextapp.echo2.app.list.ListCellRenderer;
 import org.apache.commons.lang.StringUtils;
 import org.openvpms.archetype.rules.doc.DocumentHandlers;
 import org.openvpms.archetype.rules.product.ProductPriceRules;
@@ -43,9 +49,14 @@ import org.openvpms.web.component.workspace.ResultSetCRUDWindow;
 import org.openvpms.web.echo.button.ButtonSet;
 import org.openvpms.web.echo.dialog.ConfirmationDialog;
 import org.openvpms.web.echo.dialog.InformationDialog;
+import org.openvpms.web.echo.dialog.MessageDialog;
 import org.openvpms.web.echo.dialog.PopupDialogListener;
 import org.openvpms.web.echo.event.ActionListener;
 import org.openvpms.web.echo.factory.ButtonFactory;
+import org.openvpms.web.echo.factory.ColumnFactory;
+import org.openvpms.web.echo.factory.LabelFactory;
+import org.openvpms.web.echo.factory.RowFactory;
+import org.openvpms.web.echo.factory.SelectFieldFactory;
 import org.openvpms.web.echo.help.HelpContext;
 import org.openvpms.web.resource.i18n.Messages;
 import org.openvpms.web.system.ServiceHelper;
@@ -53,6 +64,8 @@ import org.openvpms.web.workspace.product.io.ProductExportDialog;
 import org.openvpms.web.workspace.product.io.ProductImportDialog;
 import org.openvpms.web.workspace.product.io.ProductImportErrorDialog;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -189,47 +202,26 @@ public class ProductCRUDWindow extends ResultSetCRUDWindow<Product> {
         }
     }
 
+    /**
+     * Invoked when the "export" button is pressed.
+     */
     private void onExport() {
         HelpContext help = getHelpContext().subtopic("export");
         ProductExportDialog dialog = new ProductExportDialog(createLayoutContext(help), help);
         dialog.show();
     }
 
-
+    /**
+     * Invoked when the "import" button is pressed.
+     */
     private void onImport() {
         final HelpContext help = getHelpContext().subtopic("import");
         DocumentUploadListener listener = new DocumentUploadListener() {
 
             @Override
             protected void upload(Document document) {
-                ProductCSVReader importer = new ProductCSVReader(ServiceHelper.getBean(DocumentHandlers.class));
                 try {
-                    ProductDataSet data = importer.read(document);
-                    if (data.getErrors().isEmpty()) {
-                        ProductDataFilter filter = new ProductDataFilter(ServiceHelper.getBean(ProductPriceRules.class),
-                                                                         ServiceHelper.getArchetypeService());
-                        data = filter.filter(data.getData());
-                    }
-                    if (data.getErrors().isEmpty()) {
-                        final List<ProductData> output = data.getData();
-                        if (!output.isEmpty()) {
-                            ProductImportDialog dialog = new ProductImportDialog(output, help);
-                            dialog.show();
-                            dialog.addWindowPaneListener(new PopupDialogListener() {
-                                @Override
-                                public void onOK() {
-                                    onImport(output);
-                                }
-                            });
-                        } else {
-                            InformationDialog.show(Messages.get("product.import.title"),
-                                                   Messages.get("product.import.nochanges"));
-                        }
-                    } else {
-                        List<ProductData> errors = data.getErrors();
-                        ProductImportErrorDialog dialog = new ProductImportErrorDialog(errors, help.subtopic("errors"));
-                        dialog.show();
-                    }
+                    importDocument(document, help);
                 } catch (Throwable exception) {
                     ErrorHelper.show(exception);
                 }
@@ -239,10 +231,179 @@ public class ProductCRUDWindow extends ResultSetCRUDWindow<Product> {
         dialog.show();
     }
 
+    /**
+     * Imports a document.
+     *
+     * @param document the document to import
+     * @param help     the help context
+     */
+    private void importDocument(Document document, HelpContext help) {
+        ProductCSVReader reader = new ProductCSVReader(ServiceHelper.getBean(DocumentHandlers.class));
+        List<SimpleDateFormat> formats = reader.getDateFormats(document);
+        boolean ambiguousDates = false;
+        if (formats.size() > 1) {
+            List<SimpleDateFormat> dmy = getFormats(formats, ProductCSVReader.DAY_MONTH_YEAR_FORMATS);
+            List<SimpleDateFormat> mdy = getFormats(formats, ProductCSVReader.MONTH_DAY_YEAR_FORMATS);
+            List<SimpleDateFormat> ymd = getFormats(formats, ProductCSVReader.YEAR_MONTH_DAY_FORMATS);
+            List<List<SimpleDateFormat>> formatList = new ArrayList<List<SimpleDateFormat>>();
+            if (!dmy.isEmpty()) {
+                formatList.add(dmy);
+            }
+            if (!mdy.isEmpty()) {
+                formatList.add(mdy);
+            }
+            if (!ymd.isEmpty()) {
+                formatList.add(ymd);
+            }
+            if (formatList.size() > 1) {
+                ambiguousDates = true;
+                selectDateFormatAndImport(document, formatList, reader, help);
+            }
+        }
+        if (!ambiguousDates) {
+            importDocument(document, reader, help);
+        }
+    }
+
+    /**
+     * Imports a document.
+     *
+     * @param document the document to import
+     * @param reader   the document reader
+     * @param help     the help context
+     */
+    private void importDocument(Document document, ProductCSVReader reader, HelpContext help) {
+        ProductDataSet data = reader.read(document);
+        if (data.getErrors().isEmpty()) {
+            ProductDataFilter filter = new ProductDataFilter(ServiceHelper.getBean(ProductPriceRules.class),
+                                                             ServiceHelper.getArchetypeService());
+            data = filter.filter(data.getData());
+        }
+        if (data.getErrors().isEmpty()) {
+            final List<ProductData> output = data.getData();
+            if (!output.isEmpty()) {
+                ProductImportDialog dialog = new ProductImportDialog(output, help);
+                dialog.show();
+                dialog.addWindowPaneListener(new PopupDialogListener() {
+                    @Override
+                    public void onOK() {
+                        onImport(output);
+                    }
+                });
+            } else {
+                InformationDialog.show(Messages.get("product.import.title"),
+                                       Messages.get("product.import.nochanges"));
+            }
+        } else {
+            List<ProductData> errors = data.getErrors();
+            ProductImportErrorDialog dialog = new ProductImportErrorDialog(errors, help.subtopic("errors"));
+            dialog.show();
+        }
+    }
+
+    /**
+     * Prompts to select the date format, before importing the prices.
+     * <p/>
+     * This is used if multiple date formats can be used to interpret dates
+     * (e.g. 1/3/2013 could be parsed using d/m/y or m/d/y).
+     *
+     * @param document   the document to import
+     * @param formatList the possible date formats to parse dates
+     * @param reader     the document reader
+     * @param help       the help context
+     */
+    private void selectDateFormatAndImport(final Document document, final List<List<SimpleDateFormat>> formatList,
+                                           final ProductCSVReader reader, final HelpContext help) {
+        final DateFormatDialog dialog = new DateFormatDialog(formatList);
+        dialog.addWindowPaneListener(new PopupDialogListener() {
+            @Override
+            public void onOK() {
+                reader.setDateFormats(dialog.getDateFormats());
+                importDocument(document, reader, help);
+            }
+        });
+        dialog.show();
+    }
+
+    /**
+     * Returns the matches that correspond to the supplied formats.
+     * <p/>
+     * This is used to organise formats into families (i.e. d/m/y, m/d/y, y/m/d).
+     *
+     * @param matches the matches that can be used to parse the dates
+     * @param formats the formats to compare with
+     * @return the matches that appear in formats
+     */
+    private List<SimpleDateFormat> getFormats(List<SimpleDateFormat> matches, SimpleDateFormat[] formats) {
+        List<SimpleDateFormat> result = new ArrayList<SimpleDateFormat>();
+        for (SimpleDateFormat match : matches) {
+            for (SimpleDateFormat format : formats) {
+                if (match == format) {
+                    result.add(match);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Imports prices.
+     *
+     * @param data the data to import
+     */
     private void onImport(List<ProductData> data) {
         ProductImporter importer = new ProductImporter(ServiceHelper.getBean(ProductPriceRules.class),
                                                        ServiceHelper.getArchetypeService());
         importer.run(data, getContext().getPractice());
         InformationDialog.show(Messages.get("product.import.title"), Messages.get("product.import.imported"));
+    }
+
+    static class DateFormatDialog extends MessageDialog {
+
+        /**
+         * The date format field.
+         */
+        private final SelectField field;
+
+        /**
+         * Constructs a {@link DateFormatDialog}.
+         *
+         * @param formats the date formats
+         */
+        public DateFormatDialog(List<List<SimpleDateFormat>> formats) {
+            super(Messages.get("product.import.dateformat.title"), Messages.get("product.import.dateformat.message"),
+                  OK_CANCEL);
+            field = SelectFieldFactory.create(formats);
+            field.setCellRenderer(new ListCellRenderer() {
+                @Override
+                @SuppressWarnings("unchecked")
+                public Object getListCellRendererComponent(Component list, Object value, int index) {
+                    List<SimpleDateFormat> formats = (List<SimpleDateFormat>) value;
+                    return formats.get(0).toPattern().toLowerCase();
+                }
+            });
+        }
+
+        /**
+         * Returns the selected date formats.
+         *
+         * @return the date formats
+         */
+        @SuppressWarnings("unchecked")
+        public List<SimpleDateFormat> getDateFormats() {
+            return (List<SimpleDateFormat>) field.getSelectedItem();
+        }
+
+        /**
+         * Lays out the component prior to display.
+         */
+        @Override
+        protected void doLayout() {
+            Label message = LabelFactory.create();
+            message.setText(getMessage());
+            Column column = ColumnFactory.create("WideCellSpacing", message, field);
+            Row row = RowFactory.create("Inset.Large", column);
+            getLayout().add(row);
+        }
     }
 }
