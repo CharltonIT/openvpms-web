13122f2401f246c48a3f76afbd346751b4173293
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-10-15 09:11:49 +0000

    Changes for OVPMS-1367 Add support for 24/7 Till Balancing
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5490 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/reporting/till/TillBalanceActQuery.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/reporting/till/TillBalanceActQuery.java
@@ -16,12 +16,18 @@
 
 package org.openvpms.web.workspace.reporting.till;
 
-import org.openvpms.archetype.rules.finance.till.TillArchetypes;
-import org.openvpms.archetype.rules.finance.till.TillBalanceStatus;
 import org.openvpms.component.business.domain.im.act.FinancialAct;
 import org.openvpms.component.business.domain.im.common.Entity;
+import org.openvpms.web.component.im.lookup.LookupFilter;
+import org.openvpms.web.component.im.lookup.NodeLookupQuery;
 import org.openvpms.web.component.im.query.ActStatuses;
 import org.openvpms.web.component.im.query.DateRangeActQuery;
+
+import static org.openvpms.archetype.rules.finance.till.TillArchetypes.TILL_BALANCE;
+import static org.openvpms.archetype.rules.finance.till.TillArchetypes.TILL_PARTICIPATION;
+import static org.openvpms.archetype.rules.finance.till.TillBalanceStatus.CLEARED;
+import static org.openvpms.archetype.rules.finance.till.TillBalanceStatus.IN_PROGRESS;
+import static org.openvpms.archetype.rules.finance.till.TillBalanceStatus.UNCLEARED;
 
 /**
  * Query for <em>act.tillBalance</em>. acts.
@@ -33,19 +39,18 @@ public class TillBalanceActQuery extends DateRangeActQuery<FinancialAct> {
     /**
      * The short names to query.
      */
-    private static final String[] SHORT_NAMES = {TillArchetypes.TILL_BALANCE};
+    private static final String[] SHORT_NAMES = {TILL_BALANCE};
 
     /**
      * The default statuses to query.
      */
-    private static final String[] DEFAULT_STATUSES = {TillBalanceStatus.UNCLEARED, TillBalanceStatus.IN_PROGRESS};
+    private static final String[] DEFAULT_STATUSES = {UNCLEARED, IN_PROGRESS};
 
     /**
-     * The act statuses. Exclude the <em>IN_PROGRESS</em> status, as it will be handled explicitly whenever
-     * <em>UNCLEARED</em> is selected.
+     * The act statuses. This excludes the IN_PROGRESS from display.
      */
-    private static final ActStatuses STATUSES = new ActStatuses(TillArchetypes.TILL_BALANCE,
-                                                                TillBalanceStatus.IN_PROGRESS);
+    private static final ActStatuses STATUSES = new ActStatuses(
+            new LookupFilter(new NodeLookupQuery(TILL_BALANCE, "status"), true, UNCLEARED, CLEARED), null);
 
     /**
      * Constructs a {@link TillBalanceActQuery}.
@@ -53,7 +58,7 @@ public class TillBalanceActQuery extends DateRangeActQuery<FinancialAct> {
      * @param till the till to query balance acts for
      */
     public TillBalanceActQuery(Entity till) {
-        super(till, "till", TillArchetypes.TILL_PARTICIPATION, SHORT_NAMES, STATUSES, FinancialAct.class);
+        super(till, "till", TILL_PARTICIPATION, SHORT_NAMES, STATUSES, FinancialAct.class);
         setAuto(true);
     }
 
@@ -67,9 +72,10 @@ public class TillBalanceActQuery extends DateRangeActQuery<FinancialAct> {
     @Override
     protected String[] getStatuses() {
         String[] statuses = super.getStatuses();
-        if (statuses.length == 1 && statuses[0].equals(TillBalanceStatus.UNCLEARED)) {
+        if (statuses.length == 1 && statuses[0].equals(UNCLEARED)) {
             statuses = DEFAULT_STATUSES;
         }
         return statuses;
     }
+
 }
