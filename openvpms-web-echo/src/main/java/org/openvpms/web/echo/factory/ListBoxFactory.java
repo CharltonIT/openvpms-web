1464031fc25cc4e72a92bc0ab33a31c1bff693b4
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-11-19 02:09:01 +0000

    Changed ListBoxFactory to use KeyListBox instead of ListBox
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5575 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-echo/src/main/java/org/openvpms/web/echo/factory/ListBoxFactory.java
+++ openvpms-web-echo/src/main/java/org/openvpms/web/echo/factory/ListBoxFactory.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.echo.factory;
@@ -21,14 +19,14 @@ package org.openvpms.web.echo.factory;
 import nextapp.echo2.app.ListBox;
 import nextapp.echo2.app.list.DefaultListModel;
 import nextapp.echo2.app.list.ListModel;
+import org.openvpms.web.echo.list.KeyListBox;
 
 import java.util.List;
 
 /**
  * Factory for {@link ListBox}es.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-04 06:28:25Z $
+ * @author Tim Anderson
  */
 public final class ListBoxFactory extends ComponentFactory {
 
@@ -38,7 +36,7 @@ public final class ListBoxFactory extends ComponentFactory {
      * @param model the model
      */
     public static ListBox create(ListModel model) {
-        ListBox list = new ListBox(model);
+        ListBox list = new KeyListBox(model);
         setDefaultStyle(list);
         if (model.size() > 0) {
             // default to the first element
