b377ad4e2946a2d869f03e76e2539dfd78e7d627
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-01-17 06:42:53 +0000

    Updated javadoc
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@5717 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-macro/src/main/java/org/openvpms/macro/Position.java
+++ openvpms-macro/src/main/java/org/openvpms/macro/Position.java
@@ -17,28 +17,55 @@
 package org.openvpms.macro;
 
 /**
- * .
+ * Tracks the changes in cursor position, when macros are being expanded in text.
  *
  * @author Tim Anderson
  */
 public class Position {
 
+    /**
+     * The initial cursor position.
+     */
     private int oldPosition;
+
+    /**
+     * The new cursor position.
+     */
     private int newPosition;
 
+    /**
+     * Constructs a {@link Position}.
+     *
+     * @param position the initial position
+     */
     public Position(int position) {
         this.oldPosition = position;
         this.newPosition = position;
     }
 
+    /**
+     * Returns the initial cursor position.
+     *
+     * @return the initial cursor position
+     */
     public int getOldPosition() {
         return oldPosition;
     }
 
+    /**
+     * Returns the new cursor position.
+     *
+     * @return the new cursor position
+     */
     public int getNewPosition() {
         return newPosition;
     }
 
+    /**
+     * Sets the new cursor position.
+     *
+     * @param position the new cursor position
+     */
     public void setNewPosition(int position) {
         newPosition = position;
     }
