77adca2e119717b399827362b7bb28998b530997
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-03-17 23:58:02 +0000

    Change to make the label style bold
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@5806 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-component/src/main/java/org/openvpms/web/component/im/view/ComponentState.java
+++ openvpms-web-component/src/main/java/org/openvpms/web/component/im/view/ComponentState.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.view;
@@ -24,6 +24,7 @@ import org.openvpms.web.component.property.Property;
 import org.openvpms.web.echo.factory.LabelFactory;
 import org.openvpms.web.echo.focus.FocusGroup;
 import org.openvpms.web.echo.focus.FocusHelper;
+import org.openvpms.web.echo.style.Styles;
 
 
 /**
@@ -177,7 +178,7 @@ public class ComponentState {
      */
     public Label getLabel() {
         if (label == null) {
-            label = LabelFactory.create();
+            label = LabelFactory.create(null, Styles.BOLD);
             label.setText(displayName);
         }
         return label;
