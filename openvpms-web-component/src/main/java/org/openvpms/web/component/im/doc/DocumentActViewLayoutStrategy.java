92cec02377c3af433e56ddab0cc1f6b1e44c1f5a
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-05-20 23:26:52 +0000

    Change to suppress the versions node of document acts if they are empty when viewing acts
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@5938 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-component/src/main/java/org/openvpms/web/component/im/doc/DocumentActViewLayoutStrategy.java
+++ openvpms-web-component/src/main/java/org/openvpms/web/component/im/doc/DocumentActViewLayoutStrategy.java
@@ -1,17 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2008-2012 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.doc;
@@ -19,6 +19,7 @@ package org.openvpms.web.component.im.doc;
 import org.openvpms.component.business.domain.im.act.DocumentAct;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.service.archetype.helper.IMObjectBean;
+import org.openvpms.web.component.im.layout.ArchetypeNodes;
 import org.openvpms.web.component.im.layout.LayoutContext;
 import org.openvpms.web.component.im.view.ComponentState;
 import org.openvpms.web.component.property.Property;
@@ -37,11 +38,10 @@ public class DocumentActViewLayoutStrategy extends DocumentActLayoutStrategy {
      * @param property the property
      * @param parent   the parent object
      * @param context  the layout context
-     * @return a component to display <tt>property</tt>
+     * @return a component to display {@code property}
      */
     @Override
-    protected ComponentState createComponent(Property property, IMObject parent,
-                                             LayoutContext context) {
+    protected ComponentState createComponent(Property property, IMObject parent, LayoutContext context) {
         String name = property.getName();
         ComponentState result;
         if (name.equals("documentTemplate")) {
@@ -57,6 +57,22 @@ public class DocumentActViewLayoutStrategy extends DocumentActLayoutStrategy {
         return result;
     }
 
+    /**
+     * Returns {@link ArchetypeNodes} to determine which nodes will be displayed.
+     *
+     * @return the archetype nodes
+     */
+    @Override
+    protected ArchetypeNodes getArchetypeNodes() {
+        return VIEW_NODES;
+    }
+
+    /**
+     * Determines if an object has a document node.
+     *
+     * @param object the object to check
+     * @return {@code true} if the object has a document node, otherwise {@code false}
+     */
     private boolean hasDocumentNode(IMObject object) {
         IMObjectBean bean = new IMObjectBean(object);
         return bean.hasNode(DOCUMENT);
