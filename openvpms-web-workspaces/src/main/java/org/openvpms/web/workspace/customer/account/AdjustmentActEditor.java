0b4d67e43bcdde49875b03575fc9ce103b1ebeaf
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-08-10 07:05:53 +0000

    Updated due to CustomerTaxRules ctor change
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@6166 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/customer/account/AdjustmentActEditor.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/customer/account/AdjustmentActEditor.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.customer.account;
@@ -29,6 +27,7 @@ import org.openvpms.web.component.im.layout.LayoutContext;
 import org.openvpms.web.component.property.Modifiable;
 import org.openvpms.web.component.property.ModifiableListener;
 import org.openvpms.web.component.property.Property;
+import org.openvpms.web.system.ServiceHelper;
 
 import java.math.BigDecimal;
 
@@ -73,7 +72,8 @@ public class AdjustmentActEditor extends AccountActEditor {
         Context context = getLayoutContext().getContext();
         Party practice = context.getPractice();
         if (practice != null) {
-            CustomerTaxRules rules = new CustomerTaxRules(practice);
+            CustomerTaxRules rules = new CustomerTaxRules(practice, ServiceHelper.getArchetypeService(),
+                                                          ServiceHelper.getLookupService());
             BigDecimal tax = rules.calculateTax(act);
             if (tax.compareTo(previousTax) != 0) {
                 Property property = getProperty("tax");
