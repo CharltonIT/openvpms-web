2d638955e3fb01aba1ee7420ee98e8fe177dd0ec
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-09-12 02:03:37 +0000

    Integrated changes for OVPMS-1519 Relative Date extensions contributed by Tim Gething
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@6206 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-echo/src/main/java/org/openvpms/web/echo/date/RelativeDateParser.java
+++ openvpms-web-echo/src/main/java/org/openvpms/web/echo/date/RelativeDateParser.java
@@ -1,17 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.echo.date;
@@ -28,25 +28,32 @@ import java.util.regex.Pattern;
 /**
  * Relative date parser.
  * <p/>
- * Recognises strings of the form: <code>([-](0..9)+(d|m|w|y))+</code>
- * as being relative to the current date. E.g:
+ * Recognises strings of the form: <code>({+|-}(0..9)+(d|m|w|q|y)+{s|e})</code>
+ * as being relative to the current date, where d=day, w=week, m=month, q=quarter
+ * (i.e. 3 months), and y=year. The optional s or e means start or end. For weeks,
+ * start means Monday, end Friday
+ * E.g:
  * <ul>
  * <li><em>-10w3d<em> sets the date to 10 weeks and 3 days before the current
  * date</li>
- * <li><em>5y3m</em> sets the date to 5 year sand 3 montsh from the current
+ * <li><em>5y3m</em> sets the date to 5 years and 3 months from the current
  * date</li>
+ * <li><em>-1qs</em> sets the date to the start of the quarter before the current
+ * one</li>
+ * <li><em>-1ms+3d</em> sets the date to 3 days after the start of the previous month</li>
+ * <li><em>0ys</em> sets the date to the start (1 Jan) of the current year</li>
+ * <li><em>0ye-1m</em> sets the date to one month prior to the end of the current year</li>
  * </ul>
  *
  * @author Tim Anderson
+ * @author Tim Gething
  */
 public class RelativeDateParser {
 
     /**
      * The pattern.
      */
-    private final Pattern pattern
-        = Pattern.compile("\\s*(-?\\d+)([dmwy])\\s*");
-
+    private final Pattern pattern = Pattern.compile("\\s*([\\+-]?\\d+)([dmwyq])([se]?)\\s*");
 
     /**
      * Parses a date relative to the current time.
@@ -80,28 +87,65 @@ public class RelativeDateParser {
             if (start != matcher.start()) {
                 return null;
             }
-            int value = Integer.parseInt(matcher.group(1));
+            String valueGroup = matcher.group(1);
+            int value = Integer.parseInt(valueGroup);
             if (first) {
                 if (value < 0) {
                     neg = true;
                 }
                 first = false;
-            } else {
-                if (value >= 0 && neg) {
-                    // if there is a leading sign, it propagates to all
-                    // other patterns where no sign is specified
-                    value = -value;
-                }
+            } else if (value >= 0 && valueGroup.charAt(0) != '+' && neg) {
+                // if there is a leading sign, and the current value has no explicit +, it propagates to all
+                // other patterns where no sign is specified
+                value = -value;
             }
             String field = matcher.group(2);
+            String se = matcher.group(3); // get any s=start or e=end
             if (field.equals("d")) {
                 calendar.add(Calendar.DAY_OF_MONTH, value);
+                // se ignored if day
             } else if (field.equals("m")) {
                 calendar.add(Calendar.MONTH, value);
+                if (se.equals("s")) {
+                    calendar.set(Calendar.DAY_OF_MONTH, 1); // set 1st of month
+                } else if (se.equals("e")) {
+                    calendar.set(Calendar.DAY_OF_MONTH, 1);  // set 1st of month
+                    calendar.add(Calendar.MONTH, 1);         // go to next month
+                    calendar.add(Calendar.DAY_OF_MONTH, -1); // back one to to get end of month
+                }
             } else if (field.equals("w")) {
                 calendar.add(Calendar.WEEK_OF_YEAR, value);
+                if (se.equals("s")) {
+                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY); //set Monday
+                } else if (se.equals("e")) {
+                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY); //set Friday
+                }
+            } else if (field.equals("q")) {                 // quarter
+                calendar.add(Calendar.MONTH, 3 * value);    // move by 3 month blocks
+                if (se.equals("s")) {
+                    calendar.set(Calendar.DAY_OF_MONTH, 1); // set 1st of month
+                    int k = calendar.get(Calendar.MONTH);   // get month (0 = Jan)
+                    k = (k / 3) * 3;                        // get month number at start of quarter (0,3,6,9)
+                    calendar.set(Calendar.MONTH, k);        // set that month
+                } else if (se.equals("e")) {
+                    calendar.set(Calendar.DAY_OF_MONTH, 1);  // set 1st of month
+                    int k = calendar.get(Calendar.MONTH);    // get month (0 = Jan)
+                    k = (k / 3) * 3;                         // get month number at start of quarter (0,3,6,9)
+                    calendar.set(Calendar.MONTH, k);         // set that month
+                    calendar.add(Calendar.MONTH, 3);         // go to next quarter
+                    calendar.add(Calendar.DAY_OF_MONTH, -1); // back one day to to get end of quarter
+                }
             } else {
                 calendar.add(Calendar.YEAR, value);
+                if (se.equals("s")) {
+                    calendar.set(Calendar.DAY_OF_MONTH, 1);         // set 1st of month
+                    calendar.set(Calendar.MONTH, Calendar.JANUARY); // set January
+                } else if (se.equals("e")) {
+                    calendar.set(Calendar.DAY_OF_MONTH, 1);         // set 1st of month
+                    calendar.set(Calendar.MONTH, Calendar.JANUARY); // set January
+                    calendar.add(Calendar.YEAR, 1);                 // go to next year
+                    calendar.add(Calendar.DAY_OF_MONTH, -1);        // back one to to get 31 Dec
+                }
             }
 
             start = matcher.end();
--- openvpms-web-echo/src/test/java/org/openvpms/web/echo/date/RelativeDateParserTestCase.java
+++ openvpms-web-echo/src/test/java/org/openvpms/web/echo/date/RelativeDateParserTestCase.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.echo.date;
@@ -32,10 +30,15 @@ import static org.junit.Assert.assertNull;
 /**
  * Tests the {@link RelativeDateParser} class.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
+ * @author Tim Gething
  */
 public class RelativeDateParserTestCase {
+
+    /**
+     * The parser.
+     */
+    private final RelativeDateParser parser = new RelativeDateParser();
 
     /**
      * Tests the {@link RelativeDateParser#parse(String, Date)} method.
@@ -46,25 +49,31 @@ public class RelativeDateParserTestCase {
         checkEquals("1w", 0, 1, 0, 0);
         checkEquals("1m", 0, 0, 1, 0);
         checkEquals("1y", 0, 0, 0, 1);
+        checkEquals("1q", 0, 0, 3, 0);
 
         checkEquals("2D", 2, 0, 0, 0);
         checkEquals("3W", 0, 3, 0, 0);
         checkEquals("4M", 0, 0, 4, 0);
         checkEquals("5Y", 0, 0, 0, 5);
+        checkEquals("3Q", 0, 0, 9, 0);
 
         checkEquals("-1d", -1, 0, 0, 0);
         checkEquals("-1w", 0, -1, 0, 0);
         checkEquals("-1m ", 0, 0, -1, 0);
         checkEquals(" -1y", 0, 0, 0, -1);
+        checkEquals(" -1q", 0, 0, -3, 0);
 
         checkEquals("1d 2w 3m 4y ", 1, 2, 3, 4);
 
         checkEquals(" -30D-52W-12M-100Y", -30, -52, -12, -100);
+        checkEquals(" -30D+52W-12M+100Y", -30, 52, -12, 100);
 
         // verify 6m is treated as -6m due to leading minus
         checkEquals("-3y6m", 0, 0, -6, -3);
         checkEquals("-3y 6m", 0, 0, -6, -3);
         checkEquals("-3y-6m", 0, 0, -6, -3);
+        // verify +6m is treated as +6m in spite of leading minus
+        checkEquals("-3y+6m", 0, 0, 6, -3);
     }
 
     /**
@@ -84,12 +93,126 @@ public class RelativeDateParserTestCase {
     }
 
     /**
+     * Verifies that start and end variants work.
+     */
+    @Test
+    public void testStartEnd() {
+        Date date = TestHelper.getDate("2014-08-25");
+        String tests[][] = {
+                {"0ds", "2014-08-25"},
+                {"0de", "2014-08-25"},
+                {"0ws", "2014-08-25"},
+                {"0we", "2014-08-29"},
+                {"0ms", "2014-08-01"},
+                {"0me", "2014-08-31"},
+                {"0qs", "2014-07-01"},
+                {"0qe", "2014-09-30"},
+                {"0ys", "2014-01-01"},
+                {"0ye", "2014-12-31"},
+                {"-3ws", "2014-08-04"},
+                {"-3ms", "2014-05-01"},
+                {"-1qs", "2014-04-01"},
+                {"-2qs", "2014-01-01"},
+                {"-3qs", "2013-10-01"},
+                {"-4qs", "2013-07-01"},
+                {"-3ys", "2011-01-01"},
+                {"-4qs+2d", "2013-07-03"},
+                {"-3ys+4d", "2011-01-05"},
+                {"-3ys+4d+2m", "2011-03-05"},
+        };
+
+        for (String[] test : tests) {
+            checkParse(test[1], date, test[0]);
+        }
+    }
+
+    /**
+     * Verifies processing of the "q", "qs", and "qe" modifiers.
+     */
+    @Test
+    public void testQuarters() {
+        // +0 quarter
+        checkParse("2014-07-01", "2014-07-01", "0q");
+        checkParse("2014-08-25", "2014-08-25", "0q");
+        checkParse("2014-09-30", "2014-09-30", "0q");
+
+        // -1 quarter
+        checkParse("2014-04-01", "2014-07-01", "-1q");
+        checkParse("2014-05-25", "2014-08-25", "-1q");
+        checkParse("2014-06-30", "2014-09-30", "-1q");
+
+        // -4 quarter
+        checkParse("2013-07-01", "2014-07-01", "-4q");
+        checkParse("2013-08-25", "2014-08-25", "-4q");
+        checkParse("2013-09-30", "2014-09-30", "-4q");
+
+        // +1 quarter
+        checkParse("2014-10-01", "2014-07-01", "1q");
+        checkParse("2014-11-25", "2014-08-25", "1q");
+        checkParse("2014-12-30", "2014-09-30", "1q");
+
+        // +4 quarter
+        checkParse("2015-07-01", "2014-07-01", "4q");
+        checkParse("2015-08-25", "2014-08-25", "4q");
+        checkParse("2015-09-30", "2014-09-30", "4q");
+
+        // current quarter start
+        checkParse("2014-07-01", "2014-07-01", "0qs");
+        checkParse("2014-07-01", "2014-08-25", "0qs");
+        checkParse("2014-07-01", "2014-09-30", "0qs");
+
+        // current quarter end
+        checkParse("2014-09-30", "2014-07-01", "0qe");
+        checkParse("2014-09-30", "2014-08-25", "0qe");
+        checkParse("2014-09-30", "2014-09-30", "0qe");
+
+        // -1 quarter start
+        checkParse("2014-04-01", "2014-07-01", "-1qs");
+        checkParse("2014-04-01", "2014-08-25", "-1qs");
+        checkParse("2014-04-01", "2014-09-30", "-1qs");
+
+        // -1 quarter end
+        checkParse("2014-06-30", "2014-07-01", "-1qe");
+        checkParse("2014-06-30", "2014-08-25", "-1qe");
+        checkParse("2014-06-30", "2014-09-30", "-1qe");
+
+        // -4 quarter start
+        checkParse("2013-07-01", "2014-07-01", "-4qs");
+        checkParse("2013-07-01", "2014-08-25", "-4qs");
+        checkParse("2013-07-01", "2014-09-30", "-4qs");
+
+        // -4 quarter end
+        checkParse("2013-09-30", "2014-07-01", "-4qe");
+        checkParse("2013-09-30", "2014-08-25", "-4qe");
+        checkParse("2013-09-30", "2014-09-30", "-4qe");
+
+        // +1 quarter start
+        checkParse("2014-10-01", "2014-07-01", "1qs");
+        checkParse("2014-10-01", "2014-08-25", "1qs");
+        checkParse("2014-10-01", "2014-09-30", "1qs");
+
+        // +1 quarter end
+        checkParse("2014-12-31", "2014-07-01", "1qe");
+        checkParse("2014-12-31", "2014-08-25", "1qe");
+        checkParse("2014-12-31", "2014-09-30", "1qe");
+
+        // +4 quarter start
+        checkParse("2015-07-01", "2014-07-01", "4qs");
+        checkParse("2015-07-01", "2014-08-25", "4qs");
+        checkParse("2015-07-01", "2014-09-30", "4qs");
+
+        // +4 quarter end
+        checkParse("2015-09-30", "2014-07-01", "4qe");
+        checkParse("2015-09-30", "2014-08-25", "4qe");
+        checkParse("2015-09-30", "2014-09-30", "4qe");
+    }
+
+    /**
      * Tests the {@link RelativeDateParser#parse(String, Date)} method when
      * supplied with invalid strings.
      */
     @Test
     public void testInvalidParse() {
-        RelativeDateParser parser = new RelativeDateParser();
         assertNull(parser.parse(null));
         assertNull(parser.parse(""));
         assertNull(parser.parse("  "));
@@ -99,6 +222,30 @@ public class RelativeDateParserTestCase {
         assertNull(parser.parse("-1dy"));
         assertNull(parser.parse("-1z"));
         assertNull(parser.parse("1d1z"));
+        assertNull(parser.parse("1qw"));
+    }
+
+    /**
+     * Parses a string relative to date, and verifies the result matches that expected.
+     *
+     * @param expected the expected date string
+     * @param date     the date to parse relative to
+     * @param source   the string to parse
+     */
+    private void checkParse(String expected, String date, String source) {
+        checkParse(expected, TestHelper.getDate(date), source);
+    }
+
+    /**
+     * Parses a string relative to date, and verifies the result matches that expected.
+     *
+     * @param expected the expected date string
+     * @param date     the date to parse relative to
+     * @param source   the string to parse
+     */
+    private void checkParse(String expected, Date date, String source) {
+        Date value = parser.parse(source, date);
+        assertEquals("Failure for " + source, TestHelper.getDate(expected), value);
     }
 
     /**
@@ -112,7 +259,6 @@ public class RelativeDateParserTestCase {
      */
     private void checkEquals(String source, int day, int week,
                              int month, int year) {
-        RelativeDateParser parser = new RelativeDateParser();
         Date date = TestHelper.getDate("2011-04-10");
         Date relative = parser.parse(source, date);
         Calendar calendar = new GregorianCalendar();
