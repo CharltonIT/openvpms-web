343683a29f6372f1045e930d5ef6b956706f037a
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-11-21 06:07:09 +0000

    Fix for OVPMS-1541 NPE viewing a product reference
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/trunk@6327 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-component/src/main/java/org/openvpms/web/component/workspace/ResultSetCRUDWorkspace.java
+++ openvpms-web-component/src/main/java/org/openvpms/web/component/workspace/ResultSetCRUDWorkspace.java
@@ -1,17 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2010 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 package org.openvpms.web.component.workspace;
 
@@ -96,6 +96,7 @@ public abstract class ResultSetCRUDWorkspace<T extends IMObject> extends Browser
             QueryBrowser<T> browser = getBrowser();
             browser.getQuery().setValue(object.getName());
             browser.query();
+            updateResultSet();
             if (!browser.getObjects().isEmpty()) {
                 // there are objects to display. Not necessarily that just set, but attempt to select it anyway.
                 browser.setSelected(object);
