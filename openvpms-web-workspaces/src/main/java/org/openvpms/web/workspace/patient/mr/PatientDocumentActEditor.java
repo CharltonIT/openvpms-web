0550cdc6b44a56fbf46a9060a5511e427d83d7e7
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-12-11 02:39:11 +0000

    Applied changes for OVPMS-1539 "Link Patient Documents and letters to invoice items such that batch numbers can be reported", contributed by Ben Charlton
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@6345 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/patient/mr/PatientDocumentActEditor.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/patient/mr/PatientDocumentActEditor.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.patient.mr;
@@ -24,6 +22,7 @@ import org.openvpms.component.business.domain.im.act.DocumentAct;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.service.archetype.helper.ActBean;
 import org.openvpms.web.component.im.doc.DocumentActEditor;
+import org.openvpms.web.component.im.layout.IMObjectLayoutStrategy;
 import org.openvpms.web.component.im.layout.LayoutContext;
 
 
@@ -56,4 +55,15 @@ public class PatientDocumentActEditor extends DocumentActEditor {
             initParticipant("patient", context.getContext().getPatient());
         }
     }
+
+    /**
+     * Creates the layout strategy.
+     *
+     * @return a new layout strategy
+     */
+    @Override
+    protected IMObjectLayoutStrategy createLayoutStrategy() {
+        return new PatientDocumentActLayoutStrategy(getDocumentEditor(), getVersionsEditor());
+    }
+
 }
