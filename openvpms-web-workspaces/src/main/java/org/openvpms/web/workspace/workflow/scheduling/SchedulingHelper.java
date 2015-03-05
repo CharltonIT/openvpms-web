a81bcf6b92fd91d65f7a4c7345c9b56d13b2c462
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2015-03-05 03:56:18 +0000

    Fix for OVPMS-1580 Appoinments entered on non-slot boundary after 12PM assigned incorrect date/time
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@6420 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/workflow/scheduling/SchedulingHelper.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/workflow/scheduling/SchedulingHelper.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.workflow.scheduling;
@@ -89,7 +89,7 @@ public class SchedulingHelper {
             }
             GregorianCalendar calendar = new GregorianCalendar();
             calendar.setTime(time);
-            calendar.set(Calendar.HOUR, 0);
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
             calendar.set(Calendar.MINUTE, 0);
             calendar.set(Calendar.SECOND, 0);
             calendar.add(Calendar.MINUTE, nearestSlot);
