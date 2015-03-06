140e8123a0c8d733343c6fa21ed61f29934c65e2
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2015-03-06 10:41:28 +0000

    Fixed appointment browser bug where selecting an appointment would reset the query parameters
    git-svn-id: svn://svn.openvpms.org/openvpms/vpms/branches/1.8-dev@6423 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/workflow/scheduling/ScheduleBrowser.java
+++ openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/workflow/scheduling/ScheduleBrowser.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.workflow.scheduling;
@@ -330,7 +330,7 @@ public abstract class ScheduleBrowser extends AbstractBrowser<PropertySet> {
      * Helper to return the event associated with an act.
      *
      * @param act the act. May be {@code null}
-     * @return the associated event, or {@code nukl} if {@code act} is null or has been deleted
+     * @return the associated event, or {@code null} if {@code act} is null or has been deleted
      */
     public PropertySet getEvent(Act act) {
         if (act != null) {
